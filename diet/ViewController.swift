df76f97a1a4002617e9cfaf42f1778bd9f250bc0
sugimotoy <ec0133xv@gmail.com>
2021-01-01 11:52:35 +0900

    Initial Commit

---
+++ diet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diet
+//
+//  Created by 杉本靖樹 on 2021/01/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
